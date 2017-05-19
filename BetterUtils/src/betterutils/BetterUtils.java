4dcce8fb4e677759f01f4c83bb11712bc64f0fd3
Preston <psscibek@gmail.com>
2017-05-19 00:05:22 -0400

    betterutils

---
+++ BetterUtils/src/betterutils/BetterUtils.java
@@ -0,0 +1,305 @@
+package betterutils;
+
+public class BetterUtils 
+{
+	public static void print(double[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void println(double[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void print(boolean[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void println(boolean[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void print(String[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void println(String[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void print(int[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void println(int[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void print(byte[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void println(byte[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void print(char[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void println(char[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void print(float[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void println(float[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void print(long[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	public static void println(long[] string)
+	{
+		for(int i = 0; i <string.length; i++)
+		{
+			System.out.print(string[i]);
+		}
+	}
+	
+	
+	public static void print(double string)
+	{
+		System.out.print(string);
+	}
+	public static void println(double string)
+	{
+		System.out.println(string);
+	}
+	public static void print(boolean string)
+	{
+		System.out.print(string);
+	}
+	public static void println(boolean string)
+	{
+		System.out.println(string);
+	}
+	public static void print(String string)
+	{
+		System.out.print(string);
+	}
+	public static void println(String string)
+	{
+		System.out.println(string);
+	}
+	public static void print(int string)
+	{
+		System.out.print(string);
+	}
+	public static void println(int string)
+	{
+		System.out.println(string);
+	}
+	public static void print(byte string)
+	{
+		System.out.print(string);
+	}
+	public static void println(byte string)
+	{
+		System.out.println(string);
+	}
+	public static void print(char string)
+	{
+		System.out.print(string);
+	}
+	public static void println(char string)
+	{
+		System.out.println(string);
+	}
+	public static void print(float string)
+	{
+		System.out.print(string);
+	}
+	public static void println(float string)
+	{
+		System.out.println(string);
+	}
+	public static void print(long string)
+	{
+		System.out.print(string);
+	}
+	public static void println(long string)
+	{
+		System.out.println(string);
+	}
+	
+	public static void print(String string, Object... args)
+	{
+		System.out.printf(string, args);
+	}
+	public static void println(String string, Object... args)
+	{
+		System.out.printf(string + "\n", args);
+	}
+	public static void flatline()
+	{
+		String flatline = "\n"+
+"                                                                                                    \n"+
+"                              |  ||                                                                 \n"+
+"                              |||||                                                                 \n"+
+"                              |||||                                                                 \n"+
+"                              |||||                                                                 \n"+
+"                             ||||||                                                                 \n"+
+"      ||                     ||||||                                                                 \n"+
+"      |                      |||| ||                                                                \n"+
+"      ||                     |||| | |           |                                                   \n"+
+"      |||           ||       |||| |||          ||                                                   \n"+
+"     ||||            |  |    ||||||||          |||                                                  \n"+
+"     |||            ||  ||   | ||  ||          |||                                                  \n"+
+"   ||| |            ||  |||  | ||  ||      ||  |||                                                  \n"+
+"|||||| ||           ||  ||   | ||   ||    | || ||||  |                                              \n"+
+"||||    |||||       ||  || | | ||   |    || ||||||||||||||||  |                         |||         \n"+
+"  |    ||||||||    |||  || ||| ||   ||    | |||| ||||||||||||||||||||||||||||||||||||||||||         \n"+
+"             ||    ||||||| ||  ||    ||  ||  |||||| ||                                              \n"+
+"             |||   | ||||| |    |    ||  ||  ||||                                                   \n"+
+"              ||| || |||||||          || ||  ||||                                                   \n"+
+"             |  |  | ||||||           ||||   ||||                                                   \n"+
+"               || || ||||||            |||    |||                                                   \n"+
+"                |||| ||| ||             |     |||                                                   \n"+
+"                | || ||| |||                   |                                                    \n"+
+"                |||| ||| ||                                                                         \n"+
+"                 |||     ||                                                                         \n"+
+"                ||||     ||                                                                         \n"+
+"                 |||    |||                                                                         \n"+
+"                  ||     ||                                                                         \n"+
+"                  |      |                                                                          ";
+		print(flatline);
+	}
+	
+	public static void smallFlatline()
+	{
+		String flatline = "                 | -                                       \n"+
+				"                  |-|                                      \n"+ 
+				"                  |||                                       \n"+
+				"                 ||||                                       \n"+
+				"   |-            -|||-      |                               \n"+
+				"   ||       |    |||||      ||                              \n"+
+				"   ||       | |- ||| ||     ||                              \n"+
+				" ||||       | || ||| ||  -| ||                              \n"+
+				"||| |||-   || ||||-| --  ||||||||                           \n"+
+				"     -|||  -|-|||| |  |  |||||||-----------------------     \n"+
+				"        |- ||||||  -  |--- |||||                            \n"+
+				"        || |-||||      ||| ||                               \n"+
+				"         ||||| |-      ||  -|                               \n"+
+				"         --||| |-           |                               \n"+
+				"         ||| | ||                                           \n"+
+				"          ||   |                                            \n"+
+				"          ||   |                                            \n"+
+				"               -                                            ";
+		println(flatline);
+	}
+	public static void skull()
+	{
+		String skull = "                                                                                                                           \n"+
+				"	                      .,od88888888888bo,.                                                                        \n"+
+				"                           .d88888888888888888888888b.                                                                    \n"+
+				"                         .d88888888888888888888888888888b.                                                                \n"+
+				"                       .d888888888888888888888888888888888b.                                                              \n"+
+				"                     .d8888888888888888888888888888888888888b.                                                            \n"+
+				"                    d88888888888888888888888888888888888888888b                                                           \n"+
+				"                   d8888888888888888888888888888888888888888888b                                                          \n"+
+				"                  d888888888888888888888888888888888888888888888                                                          \n"+
+				"                  8888888888888888888888888888888888888888888888                                                          \n"+
+				"                  8888888888888888888888888888888888888888888888                                                          \n"+
+				"                  8888888888888888888888888888888888888888888888                                                          \n"+
+				"                  Y88888888888888888888888888888888888888888888P                                                          \n"+
+				"                  \"8888888888P'   \"Y8888888888P\"    \"Y888888888\"                                                     \n"+
+				"                   88888888P        Y88888888P        Y88888888                                                           \n"+
+				"                   Y8888888          ]888888P          8888888P                                                           \n"+
+				"                    Y888888          d888888b          888888P                                                            \n"+
+				"                     Y88888b        d88888888b        d88888P                                                             \n"+
+				"                      Y888888b.   .d88888888888b.   .d888888                                                              \n"+
+				"                       Y8888888888888888P Y8888888888888888                                                               \n"+
+				"                        888888888888888P   Y88888888888888                                                                \n"+
+				"                        \"8888888888888[     ]888888888888\"                                                              \n"+
+				"                           \"Y888888888888888888888888P\"                                                                 \n"+
+				"                                \"Y88888888888888P\"                                                                      \n"+
+				"                             888b  Y8888888888P  d888                                                                     \n"+
+				"                             \"888b              d888\"                                                                   \n"+
+				"                              Y888bo.        .od888P                                                                      \n"+
+				"                               Y888888888888888888P                                                                       \n"+
+				"                                \"Y88888888888888P\"                                                                      \n"+
+				"                                  \"Y8888888888P\"                                                                        \n"+
+				"          d8888bo.                  \"Y888888P\"                  .od888b                                                 \n"+
+				"         888888888bo.                  \"\"\"\"                  .od8888888                                               \n"+
+				"         \"88888888888b.                                   .od888888888[                                                  \n"+
+				"         d8888888888888bo.                              .od888888888888                                                   \n"+
+				"       d88888888888888888888bo.                     .od8888888888888888b                                                  \n"+
+				"       ]888888888888888888888888bo.            .od8888888888888888888888b=                                                \n"+
+				"       888888888P\" \"Y888888888888888bo.     .od88888888888888P\" \"Y888888P=                                            \n"+
+				"        Y8888P\"           \"Y888888888888bd888888888888P\"            \"Y8P                                              \n"+
+				"          \"\"                   \"Y8888888888888888P\"                                                                   \n"+
+				"                                 .od8888888888bo.                                                                         \n"+
+				"                             .od888888888888888888bo.                                                                     \n"+
+				"                         .od8888888888P\"  \"Y8888888888bo.                                                               \n"+
+				"                      .od8888888888P\"        \"Y8888888888bo.                                                            \n"+
+				"                  .od88888888888P\"              \"Y88888888888bo.                                                        \n"+
+				"        .od888888888888888888P\"                    \"Y8888888888888888bo.                                                \n"+
+				"       Y8888888888888888888P\"                         \"Y8888888888888888b=                                              \n"+
+				"       888888888888888888P\"                            \"Y8888888888888888=                                              \n"+
+				"        \"Y888888888888888      					         \"Y88888888888888P=                                          \n"+
+				"             \"\"Y8888888P                                  \"Y888888P\"                                                 \n"+
+				"                \"Y8888P                                     Y888P\"                                                      \n"+
+				"                   \"\"                                        \"\"\"                                                \n"+
+				"                                                                                                                          ";
+				println(skull);
+	}
+}
---
+++ BetterUtils/src/betterutils/testing.java
@@ -0,0 +1,11 @@
+package betterutils;
+import betterutils.BetterUtils;
+public class testing 
+{
+	public static void main(String[] args)
+	{
+		System.out.printf("%d, %.2f \n", 1, 2.3333);
+		
+		BetterUtils.println("%d, %.2f", 1, 2.3333);
+	}
+}
